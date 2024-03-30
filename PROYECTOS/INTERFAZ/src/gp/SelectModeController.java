91072a30ab86727cdfb3b473f7347bb0d29f0e7e
Álvaro Enol Alonso Ortega <alvaroea@ucm.es>
2024-03-30 19:27:31 +0000

    siguiendo formato de interfaz, nueva interfaz de superfichas empezada

--- PROYECTOS/INTERFAZ/src/gp/SelectModeController.java
+++ PROYECTOS/INTERFAZ/src/gp/SelectModeController.java
@@ -57,7 +57,7 @@ public class SelectModeController {
 
     @FXML
     void switchToTableroNormal(MouseEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("TABLERO BÁSICO.fxml"));
+		root = FXMLLoader.load(getClass().getResource("TABLERO MEJOR DISEÑO.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -102,16 +102,17 @@ public class SelectModeController {
     @FXML
     void onMouseEntered(MouseEvent event) {
         // Código para el efecto al entrar con el mouse
-        Node node = (Node) event.getSource();
-        node.setStyle("-fx-background-color: lightgray;");
+        Button button = (Button) event.getSource();
+        button.setOpacity(0.35); // Cambiar la opacidad para simular una luz encendida
     }
 
     @FXML
     void onMouseExited(MouseEvent event) {
         // Código para revertir el efecto al salir con el mouse
-        Node node = (Node) event.getSource();
-        node.setStyle("-fx-background-color: transparent;");
+        Button button = (Button) event.getSource();
+        button.setOpacity(0.0); // Restaurar la opacidad original para apagar la "luz"
     }
+
     @FXML
     void switchToScene2(MouseEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("SEGUNDA PORTADA.fxml"));
